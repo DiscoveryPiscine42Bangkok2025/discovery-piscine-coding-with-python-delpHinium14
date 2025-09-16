cc0e1caa2b752184306c67f8b97fc6a489a5cf7c
a <tanadechkaewwichit@gmail.com>
2025-09-16 09:16:19 +0700

    Create iszero.py

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+try:
+    num = int(input(""))
+    if (num == 0):
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+except:
+    print("Error: invalid input.")
