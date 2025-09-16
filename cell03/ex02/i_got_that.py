472a4a92a8e6b1bb7b8d3e94ec81eb01a7a0c847
a <tanadechkaewwichit@gmail.com>
2025-09-16 09:38:29 +0700

    Create i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+def main():
+    input("What you gotta say? : ")
+
+    while True:
+        message = input("I got that! Anything else? : ")
+        if (message == "STOP"):
+            break
+
+main()
