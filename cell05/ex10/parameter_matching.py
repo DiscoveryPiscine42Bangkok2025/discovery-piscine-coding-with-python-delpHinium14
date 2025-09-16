9064886b62b45cdcdf3a1ea0fa0908a66378c077
a <tanadechkaewwichit@gmail.com>
2025-09-16 15:41:33 +0700

    Create parameter_matching.py

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,19 @@
+import sys   # นำเข้าโมดูล sys เพื่อใช้เข้าถึง argument ที่ส่งมาตอนรันโปรแกรม
+
+def main():
+    # เช็คว่ามี argument ที่ส่งมาตอนรันโปรแกรมหรือไม่ (ต้องมี 1 argument พอดี) ถ้าไม่ใช่ → จบการทำงาน
+    if len(sys.argv) != 2:
+        print("none")
+        return   
+
+    param = sys.argv[1] # เก็บค่าพารามิเตอร์ที่ผู้ใช้ส่งมาตอนรันโปรแกรม (argument ตัวที่ 1)
+
+    user_input = input("What was the parameter? ")     # ขอให้ผู้ใช้พิมพ์สิ่งที่คิดว่าเป็น parameter
+
+    if user_input == param:
+        print("Good job!")          # ถ้าตรง
+    else:
+        print("Nope, sorry...")     # ถ้าไม่ตรง
+
+if __name__ == "__main__":
+    main()
