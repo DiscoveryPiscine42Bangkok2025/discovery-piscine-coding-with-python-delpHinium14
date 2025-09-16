a0d93c3e02273731ba151a424ebe63dd15ccaae8
a <tanadechkaewwichit@gmail.com>
2025-09-16 19:06:21 +0700

    Create downcase_all.py

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,17 @@
+import sys
+
+def downcase_it(s: str) -> str:
+    return s.lower()
+
+def main():
+    params = sys.argv[1:]
+
+    if not params:
+        print("none")
+        return
+
+    for p in params:
+        print(downcase_it(p))
+
+if __name__ == "__main__":
+    main()
